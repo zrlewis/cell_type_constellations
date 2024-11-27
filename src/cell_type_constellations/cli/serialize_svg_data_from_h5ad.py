a9fbcf77cb684af370f09d4b5da048e9baa13598
danielsf <scott.daniel@alleninstitute.org>
2024-11-27 10:36:04 -0800

    add docstrings to svg cache creator

--- src/cell_type_constellations/cli/serialize_svg_data_from_h5ad.py
+++ src/cell_type_constellations/cli/serialize_svg_data_from_h5ad.py
@@ -19,33 +19,44 @@ from cell_type_constellations.svg.serialize_svg_data import (
 
 def main():
 
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        description=(
+            "Command line tool to create the HDF5 file "
+            "encoding the data necessary to visualize "
+            "different configurations of a constellation "
+            "plot associated with a given taxonomy."
+        )
+    )
 
     parser.add_argument(
         '--h5ad_path',
         type=str,
         default=None,
-        help='Path to the h5ad file'
+        help='Path to the h5ad file from which data will be read'
     )
     parser.add_argument(
         '--hierarchy',
         type=str,
         nargs='+',
         default=None,
-        help='Hierarchy of taxonomy levels'
+        help=(
+            'List the columns in obs that define '
+            'the cell type taxonomy, from most '
+            'gross to most fine '
+            '(e.g. class_label subclass_label '
+            'supertype_label cluster_label)'
+        )
     )
     parser.add_argument(
         '--color_by_columns',
         type=str,
         nargs='+',
         default=None,
-        help='Columns in obs by which to color nodes'
-    )
-    parser.add_argument(
-        '--tmp_dir',
-        type=str,
-        default=None,
-        help='Dir where scratch files can be written'
+        help=(
+            'List the columns in obs by which you want '
+            'to be able to color nodes in the constellation '
+            'plot (default=None)'
+        )
     )
     parser.add_argument(
         '--dst_path',
@@ -59,7 +70,8 @@ def main():
         default=None,
         help=(
             "Key in obsm from which the visualization "
-            "coordinates will be read"
+            "coordinates will be read [must point to an "
+            "array of shape (n_cells, 2)]"
         )
     )
     parser.add_argument(
@@ -68,26 +80,46 @@ def main():
         default=None,
         help=(
              "Key in obsm form which the connection "
-             "coordinates will be read"
+             "coordinates will be read (these are the "
+             "coordinates of the latent space in which "
+             "the connection strength between nodes in "
+             "the constellation plot will be calculated; "
+             "the higher the dimensionality of the latent "
+             "space, the longer it will take to create "
+             "the svg cache file)"
         )
     )
     parser.add_argument(
-        '--fov_factor',
+        '--fov_height',
         type=int,
         default=1080,
         help=(
-            "baseline dimension for FOV"
+            "The height in pixels of the visualization"
         )
     )
     parser.add_argument(
         '--clobber',
         default=False,
-        action='store_true'
+        action='store_true',
+        help=(
+           "Run with this option turned on if you want to "
+           "overwrite an existing output file at dst_path"
+        )
     )
     parser.add_argument(
         '--taxonomy_name',
         default=None,
-        type=str
+        type=str,
+        help=(
+            "A human-readable name associated with the "
+            "taxonomy you are visualizating"
+        )
+    )
+    parser.add_argument(
+        '--tmp_dir',
+        type=str,
+        default=None,
+        help="Directory where scratch files can be written"
     )
     args = parser.parse_args()
 
@@ -104,7 +136,7 @@ def main():
             tmp_dir=tmp_dir,
             color_by_columns=args.color_by_columns,
             clobber=args.clobber,
-            fov_factor=args.fov_factor,
+            fov_height=args.fov_height,
             taxonomy_name=args.taxonomy_name
         )
     finally:
@@ -122,7 +154,7 @@ def svg_worker(
         tmp_dir,
         color_by_columns,
         clobber,
-        fov_factor,
+        fov_height,
         taxonomy_name):
     t0 = time.time()
     dst_path = pathlib.Path(dst_path)
@@ -155,7 +187,7 @@ def svg_worker(
     write_out_svg_cache(
         src_path=data_cache_path,
         dst_path=dst_path,
-        fov_factor=fov_factor,
+        fov_factor=fov_height,
         clobber=clobber,
         taxonomy_name=taxonomy_name,
         tmp_dir=tmp_dir,
