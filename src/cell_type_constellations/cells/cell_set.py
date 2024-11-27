6bd89bb1876b1ce17f42e0547f29c8ecf7952be8
danielsf <scott.daniel@alleninstitute.org>
2024-11-27 14:27:37 -0800

    document cell_set.py

--- src/cell_type_constellations/cells/cell_set.py
+++ src/cell_type_constellations/cells/cell_set.py
@@ -1,3 +1,26 @@
+"""
+This module defines a classes that provide access to sets of cells
+(as opposed to cell type taxonomies). A valid class must inherit from the
+CellSetAccessMixin in order to provide the access pattern expected by
+other parts of the codebase. A valid class must also define an __init__
+that
+
+- sets self._visualization_coords to contain the 2-dimensional coordinates
+in which the constellation plot will be visualized
+
+- sets self._connection_coords to contain the coordinates against which
+connectivity between nodes of the constellation plot will be calculated
+
+- sets self._cluster_aliases to an (n_cells,) array indicating
+the cluster alias to which each cell is assigned
+
+- sets self._color_by_columns either to None (there are no aggregate
+statistics associated with this CellSet) or to a dict mapping
+the aggregate statistic columns from obs to (n_cells,) arrays of the
+per-cell value associated with those statistics.
+
+- calls super().__init__() to invoke the __init__ of CellSetAccessMixin
+"""
 import anndata
 import h5py
 import multiprocessing
@@ -30,10 +53,17 @@ class CellSetAccessMixin(object):
 
         n_vis = self._visualization_coords.shape[0]
         n_conn = self._connection_coords.shape[0]
+        n_alias = self._cluster_aliases.shape[0]
         if n_vis != n_conn:
             raise RuntimeError(
                 f"visualization_coords have {n_vis} points; "
                 f"connection_coords have {n_conn} points; "
+                "must be equal"
+            )
+        if n_vis != n_alias:
+            raise RuntimeError(
+                f"visualization_coords have {n_vis} points; "
+                f"there are {n_alias} cluster aliases; "
                 "must be equal"
             )
 
@@ -45,18 +75,33 @@ class CellSetAccessMixin(object):
 
     @property
     def cluster_aliases(self):
+        """
+        The (n_cells,) array of cluster alias values
+        """
         return np.copy(self._cluster_aliases)
 
     @property
     def visualization_coords(self):
+        """
+        The (n_cells, 2) array of coordinates in which the
+        constellation plot will be visualized
+        """
         return np.copy(self._visualization_coords)
 
     @property
     def connection_coords(self):
+        """
+        The (n_cells, N) array of coordinates in which connection
+        strength between nodes will be calculated
+        """
         return np.copy(self._connection_coords)
 
     @property
     def color_by_columns(self):
+        """
+        The list of aggregate statistics carried by this
+        CellSet (could be None)
+        """
         return self._color_by_columns
 
     def create_neighbor_cache(
@@ -64,6 +109,11 @@ class CellSetAccessMixin(object):
             k_nn,
             n_processors=4,
             tmp_dir=None):
+        """
+        Cache an (n_cells, k_nn) array storing, for each
+        cell in this CellSet, the indexes of its k_nn nearest
+        neighbors in connection coordinates.
+        """
 
         if k_nn in self._neighbor_cache:
             return
@@ -78,6 +128,26 @@ class CellSetAccessMixin(object):
         self._neighbor_cache[k_nn] = cache
 
     def get_connection_nn(self, query_data, k_nn):
+        """
+        Return the indexes of the k_nn nearest neighbor (in
+        connection_coordinates) cells of the cells specified
+        in query_data
+
+        Parameters
+        ----------
+        query_data:
+            a (n_query, N) array of points, where N is the
+            dimensionality of the connection_coordinate space
+        k_nn:
+            the number of nearest neighbors to find for each
+            cell in query_data
+
+        Returns
+        -------
+        A (n_query, k_nn) array of ints indicating which cells
+        (indexed, for instance, in self.cluster_aliases) are the
+        nearest neighbors of the query data.
+        """
         results = self.kd_tree.query(
             x=query_data,
             k=k_nn)
@@ -85,8 +155,24 @@ class CellSetAccessMixin(object):
 
     def get_connection_nn_from_mask(self, query_mask, k_nn):
         """
-        query_mask is a boolean mask indicating which
-        cells within self we are qureying the neighbors of
+        Return the indexes of the k_nn nearest neighbor (in
+        connection_coordinates) cells of cells within this
+        CellSet specified by query_mask
+
+        Parameters
+        ----------
+        query_mask:
+            A (n_cells,) array of booleans marked True for
+            every cell whose neighbors we want
+        k_nn:
+            The number of nearest neighbors to find for each
+            cell
+
+        Returns
+        -------
+        A (n_true, k_nn) array of ints indicating the nearest
+        neighbors for each of the cells marked True in
+        query_mask
         """
         if k_nn in self._neighbor_cache:
             query_idx = np.where(query_mask)[0]
@@ -97,6 +183,12 @@ class CellSetAccessMixin(object):
                 k_nn=k_nn)
 
     def mask_from_alias_array(self, alias_array):
+        """
+        For an array of alias values, assemble and return
+        a (n_cells, ) array of booleans marked True for each
+        cell in this CellSet that is a member of one of the aliases
+        in alias_array
+        """
         mask = np.zeros(self._cluster_aliases.shape, dtype=bool)
         for alias in alias_array:
             mask[self._cluster_aliases == alias] = True
@@ -106,6 +198,16 @@ class CellSetAccessMixin(object):
         return mask
 
     def centroid_from_alias_array(self, alias_array):
+        """
+        For an array of alias values, assemble and return
+        the visualizaton coordinates of a candidate centroid
+        of the collection of all cells assigned to any of those
+        aliases.
+
+        The centroid is chosen to be the coordinates of the
+        cell nearest the median location in 2D space of the
+        collection of cells being considered.
+        """
         mask = self.mask_from_alias_array(alias_array)
         pts = self._visualization_coords[mask, :]
         median_pt = np.median(pts, axis=0)
@@ -113,18 +215,27 @@ class CellSetAccessMixin(object):
         nn_idx = np.argmin(ddsq)
         return pts[nn_idx, :]
 
-    def color_lookup_from_alias_array(self, alias_array):
-        if self.color_by_columns is None:
-            raise RuntimeError("_color_by_columns is None")
-
-        mask = self.mask_from_alias_array(alias_array)
-        result = {
-            col_key: np.mean(self.color_by_columns[col_key][mask])
-            for col_key in self.color_by_columns
-        }
-        return result
-
     def stat_lookup_from_alias_array(self, alias_array):
+        """
+        For an array of alias values, assemble and return
+        a dict of aggregate statistics representing all
+        of the cells annotated to belong to one of those
+        aliases.
+
+        The returned dict will look something like
+        {
+            'statA': {
+                'mean': 0.1,
+                'variance': 0.02
+            },
+            'statB': {
+                'mean': 0.4,
+                'variance': 0.03
+            },
+            ...
+        }
+
+        """
         if self.color_by_columns is None:
             raise RuntimeError("_color_by_columns is None")
 
@@ -141,6 +252,11 @@ class CellSetAccessMixin(object):
         return result
 
     def n_cells_from_alias_array(self, alias_array):
+        """
+        For an array of alias values, return the number of
+        cells in this CellSet annotated to belong to one
+        of those aliases.
+        """
         mask = np.zeros(self._cluster_aliases.shape, dtype=bool)
         for alias in alias_array:
             mask[self._cluster_aliases == alias] = True
@@ -148,6 +264,9 @@ class CellSetAccessMixin(object):
 
 
 class CellSet(CellSetAccessMixin):
+    """
+    Define a CellSet based solely on a cell_metadata.csv file
+    """
 
     def __init__(
             self,
@@ -164,6 +283,29 @@ class CellSet(CellSetAccessMixin):
 
 
 class CellSetFromH5ad(CellSetAccessMixin):
+    """
+    Define a CellSet from an h5ad file.
+
+    Parameters
+    ----------
+        h5ad_path:
+            path to the h5ad file
+        visualization_coord_key:
+            the field in obsm from which visualization
+            coordinates will be read (these are the coordinates
+            in which the constellation plot will be visualized)
+        connection_coord_key:
+            the field in obsm from which connection coordinates
+            will be read (these are the coordinates which will
+            be used to calculate the strength of connections
+            between nodes in the cell type taxonomy)
+        cluster_alias_key:
+            the column in obs where cluster aliases are stored
+            (assumed to be integers)
+        color_by_columns:
+            optional list of columns in obs that will be gathered
+            as aggregate statistics over cell types.
+    """
 
     def __init__(
             self,
@@ -199,6 +341,13 @@ class CellSetFromH5ad(CellSetAccessMixin):
 
 
 def _get_umap_coords(cell_metadata_path):
+    """
+    Read in a cell_metadata.csv file.
+
+    Return an array of cluster_aliases and an array
+    of UMAP coordinates representing the cells in that
+    file.
+    """
 
     cell_metadata = pd.read_csv(cell_metadata_path)
     umap_coords = np.array(
@@ -247,6 +396,34 @@ def create_neighbor_cache(
         k_nn,
         n_processors=4,
         tmp_dir=None):
+    """
+    Create and return an (n_cells, k_nn) array storing, for each
+    cell in this CellSet, the indexes of its k_nn nearest
+    neighbors in a specified coordinate system.
+
+    Parameters
+    ----------
+    tree:
+        a scipy.spatial.cKDTree already created
+        with the candidate neighbor points
+    pts:
+        an array, each row of which is a point
+        whose neighbors we want to find
+    k_nn:
+        an int. The number of nearest neighbors to
+        find per point
+    n_processors:
+        the number of independent worker processes
+        to spin up
+    tmp_dir:
+        path to a directory where scratch files can be written
+
+    Returns
+    -------
+    A (pts.shape[0], k_nn) array of integers indicating the indexes
+    (relative to the array of points loaded into tree) of the k_nn
+    nearest neighbors of each point in pts
+    """
 
     tmp_dir = tempfile.mkdtemp(dir=tmp_dir)
     try:
