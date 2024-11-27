8015d4fb705991d72b5c4daacd3b2c42eebc54bc
danielsf <scott.daniel@alleninstitute.org>
2024-11-27 14:42:46 -0800

    document taxonomy_filter.py

--- src/cell_type_constellations/cells/taxonomy_filter.py
+++ src/cell_type_constellations/cells/taxonomy_filter.py
@@ -1,3 +1,12 @@
+"""
+This module defines a class, TaxonomyFilter, which the rest of the code
+uses to manipulate a cell type taxonomy.
+
+Most of its work is actually done by the TaxonomyTree class defined here
+
+https://github.com/AllenInstitute/cell_type_mapper/blob/main/src/cell_type_mapper/taxonomy/taxonomy_tree.py
+"""
+
 import anndata
 import numpy as np
 
@@ -7,6 +16,15 @@ from cell_type_mapper.taxonomy.taxonomy_tree import (
 
 
 class TaxonomyFilter(object):
+    """
+    A class to encapsulate and manipulate a cell_type_taxonomy.
+
+    Do not instantiate it by calling __init__. Use one of the
+    factory class methods:
+
+    TaxonomyFilter.from_data_release()
+    TaxonomyFilter.from_h5ad()
+    """
 
     def __init__(
             self,
@@ -93,6 +111,33 @@ class TaxonomyFilter(object):
             cluster_membership_path,
             hierarchy,
             cell_metadata_path=None):
+        """
+        Instantiate a TaxonomyFilter from the set of CSV files that
+        come with an ABC Atlas data release.
+
+        Parameters
+        ----------
+        cluster_annotation_path:
+            path to the cluster_annotation_term.csv file
+        cluster_membership_path:
+            path to the cluster_to_cluster_annotation_membership.csv
+            file
+        hierarchy:
+            list of the levels in the cell type taxonomy from
+            most gross to most fine
+        cell_metadata_path:
+            optional path to the cell_metadata.csv file
+            (only specified if we are considering a subset
+            of the taxonomy defined by cluster_annotation_term.sv
+            and cluster_to_cluster_annotation_membership.csv;
+            in that case, only cell type taxons annotated to
+            contain cells according to cell_metadata.csv will
+            be retained)
+
+        Returns
+        -------
+        a TaxonomyFilter
+        """
 
         taxonomy_tree = TaxonomyTree.from_data_release(
             cell_metadata_path=cell_metadata_path,
@@ -109,6 +154,24 @@ class TaxonomyFilter(object):
             h5ad_path,
             column_hierarchy,
             cluster_alias_column):
+        """
+        Instantiate a TaxonomyFilter from an H5AD file.
+
+        Parameters
+        ----------
+        h5ad_path:
+            path to the h5ad file from which data will be read
+        column_hierarchy:
+            the columns of obs that define the levels of the
+            cell type taxonomy, ordered from most gross to most fine
+        cluster_alias_column:
+            the column in obs that contains the cluster_alias
+            (assumed to be an integer)
+
+        Returns
+        -------
+        a TaxonomyFilter
+        """
 
         raw_taxonomy_tree = TaxonomyTree.from_h5ad(
              h5ad_path=h5ad_path,
@@ -158,9 +221,27 @@ class TaxonomyFilter(object):
             level,
             node):
         """
-        Given an array of cluster aliases and a specified level, node
+        Given an array of cluster aliases and a specified (level, node)
         pair, return a boolean mask indicating which of the cells
         specified by cluster_aliases are in the specified node, level
+
+        Parameters
+        ----------
+        alias_array:
+            an (n_cells,) array of alias values, each representing
+            a different cell and the alias of the cluster to which
+            it has been assigned
+        level:
+            a str. The level of the taxonomy we are considering
+        node:
+            a str. The specific node with level that we are considering.
+
+        Returns
+        -------
+        an (n_cells,) array of booleans marked True for every cell that,
+        based on the link between cluser aliases and the rest of the
+        taxonomy, is annotated to belong to the cell type taxon
+        (level, node)
         """
 
         desired_aliases = self._parentage_to_alias[level][node]
@@ -173,14 +254,32 @@ class TaxonomyFilter(object):
         return mask
 
     def idx_from_label(self, level, node):
+        """
+        Given a (level, node) pair, what is the index of that
+        taxon (for interpreting rows, columns of mixture matrices)
+        """
         return self._name_to_idx[level]['label'][node]
 
     def idx_array_from_alias_array(self, alias_array, level):
+        """
+        Given an array of cluster aliases, each representing a cell,
+        and a level in the cell type taxonomy, what index in that
+        level does each cell correspond to (for interpreting
+        rows, columns of mixture matrices)
+        """
         return self._alias_to_idx[level][alias_array]
 
     def name_from_idx(self, level, idx):
         """
-        Returns a dict with 'name' and 'label' fields
+        Given a level in the cell tyep taxonomy and an index
+        at that level (a row or column index in a mixture matrx),
+        return a dict with 'name' and 'label' fields.
+
+        'name' is the human-readable name of the corresponding
+        cell type taxon
+
+        'label'is the machine-readable, unique name of the corresponding
+        cell type taxon
         """
         result = {
             'label': self._idx_to_name[level]['label'][idx],
@@ -189,6 +288,11 @@ class TaxonomyFilter(object):
         return result
 
     def alias_array_from_idx(self, level, idx):
+        """
+        Given a level of the cell type taxonomy and and index at that
+        level (the row or column index of a mixture matrix), return
+        the list of cluster aliases corresponding to that index.
+        """
         naming = self.name_from_idx(level=level, idx=idx)
         alias = self._parentage_to_alias[level][naming['label']]
         return alias
