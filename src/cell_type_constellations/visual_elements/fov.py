224870f4cc278de8ed6a852568ccdd0875561e8d
danielsf <scott.daniel@alleninstitute.org>
2025-02-15 15:41:43 -0800

    refactor __init__ for FieldOfView to facilitate serialization

--- src/cell_type_constellations/visual_elements/fov.py
+++ src/cell_type_constellations/visual_elements/fov.py
@@ -14,22 +14,23 @@ class FieldOfView(object):
 
     def __init__(
             self,
-            embedding_bounds,
+            embedding_to_pixel,
             fov_height,
+            fov_width,
             max_radius,
             min_radius):
         """
         Parameters
         ----------
-        embedding_bounds:
-            a numpy array containing the bounds, in embedding
-            space, of the field of view.
-                [[xmin, xmax], [ymin, ymax]]
+        embedding_to_pixel:
+            3x3 array used to transform
+            [embedding_x, embedding_y, 1]
+            to
+            [pixel_x, pixel_y, 1]
         fov_height:
             The height (in pixels) of the field of view
-            (width will be calculated to preserve the aspect
-            ratio of the embedding coordinate array as encoded
-            in embedding_bounds)
+        fov_width:
+            The width (in pixels) of the field of view
         max_radius:
             The maximum radius (in pixel coordinates)
             of a node in the constellation plot
@@ -38,30 +39,12 @@ class FieldOfView(object):
             of a node in the constellation plot
         """
         self._fov_height = fov_height
-        xmax = embedding_bounds[0, 1]
-        xmin = embedding_bounds[0, 0]
-
-        ymax = embedding_bounds[1, 1]
-        ymin = embedding_bounds[1, 0]
-        dx = 6*max_radius + xmax - xmin
-        dy = 6*max_radius + ymax - ymin
-
-        ratio = dx/dy
-        self._fov_width = np.round(fov_height*ratio).astype(int)
+        self._fov_width = fov_width
 
         self._max_radius = max_radius
         self._min_radius = min_radius
 
-        pixel_buffer = 3*max_radius//2
-        self.pixel_origin = np.array([pixel_buffer, pixel_buffer])
-        self.pixel_extent = np.array([self.width-2*pixel_buffer,
-                                      self.height-2*pixel_buffer])
-
-        # origin and extent in embedding coordinates
-        self.embedding_origin = [xmin, ymin]
-        self.embedding_extent = [xmax-xmin, ymax-ymin]
-
-        self._set_embedding_to_pixel()
+        self._embedding_to_pixel = embedding_to_pixel
 
     @classmethod
     def from_h5ad(
@@ -131,11 +114,71 @@ class FieldOfView(object):
             [coords[:, 0].min(), coords[:, 0].max()],
             [coords[:, 1].min(), coords[:, 1].max()]
         ])
-        return cls(
+        return cls.from_embedding_bounds(
             embedding_bounds=bounds,
             fov_height=fov_height,
             max_radius=max_radius,
             min_radius=min_radius
+        )
+
+    @classmethod
+    def from_embedding_bounds(
+           cls,
+           embedding_bounds,
+           fov_height,
+           max_radius,
+           min_radius):
+        """
+        Parameters
+        ----------
+        embedding_bounds:
+            2x2 numpy array that is
+            [[xmin, xmax],
+             [ymin, ymax]]
+            of embedding space coordinates in this field of view
+        fov_height:
+            The height (in pixels) of the field of view
+        max_radius:
+            The maximum radius (in embedding coordinates)
+            of a node in the constellation plot
+        min_radius:
+            The maximum radius (in embedding coordinates)
+            of a node in the constellation plot
+        """
+
+        xmax = embedding_bounds[0, 1]
+        xmin = embedding_bounds[0, 0]
+
+        ymax = embedding_bounds[1, 1]
+        ymin = embedding_bounds[1, 0]
+        dx = 6*max_radius + xmax - xmin
+        dy = 6*max_radius + ymax - ymin
+
+        ratio = dx/dy
+        fov_width = np.round(fov_height*ratio).astype(int)
+
+        pixel_buffer = 3*max_radius//2
+        pixel_origin = np.array([pixel_buffer, pixel_buffer])
+        pixel_extent = np.array([fov_width-2*pixel_buffer,
+                                 fov_height-2*pixel_buffer])
+
+        # origin and extent in embedding coordinates
+        embedding_origin = [xmin, ymin]
+        embedding_extent = [xmax-xmin, ymax-ymin]
+
+        embedding_to_pixel = get_embedding_to_pixel(
+            pixel_origin=pixel_origin,
+            pixel_extent=pixel_extent,
+            embedding_origin=embedding_origin,
+            embedding_extent=embedding_extent
+        )
+
+        return cls(
+            fov_height=fov_height,
+            fov_width=fov_width,
+            max_radius=max_radius,
+            min_radius=min_radius,
+            embedding_to_pixel=embedding_to_pixel
         )
 
     @property
@@ -176,27 +219,6 @@ class FieldOfView(object):
         an [px, py, 1] vector in pixel coordinates
         """
         return self._embedding_to_pixel
-
-    def _set_embedding_to_pixel(self):
-        """
-        Set the transformation matrix for converting from
-        embedding coordinates to pixel coordinates.
-        """
-
-        e0 = self.pixel_extent[0]
-        e1 = self.pixel_extent[1]
-        we0 = self.embedding_extent[0]
-        we1 = self.embedding_extent[1]
-        p0 = self.pixel_origin[0]
-        p1 = self.pixel_origin[1]
-        w0 = self.embedding_origin[0]
-        w1 = self.embedding_origin[1]
-
-        self._embedding_to_pixel = np.array([
-            [e0/we0, 0.0, p0-e0*w0/we0],
-            [0.0, -e1/we1, p1+e1*(w1+we1)/we1],
-            [0.0, 0.0, 1.0]
-        ])
 
     def transform_to_pixel_coordinates(
             self,
@@ -253,3 +275,31 @@ class FieldOfView(object):
         logarithmic_r = np.log2(1.0+n_cells_array/n_cells_max)
         pixel_radii = self.min_radius+dr*logarithmic_r
         return pixel_radii
+
+
+def get_embedding_to_pixel(
+        pixel_origin,
+        pixel_extent,
+        embedding_origin,
+        embedding_extent):
+    """
+    Set the transformation matrix for converting from
+    embedding coordinates to pixel coordinates.
+    """
+
+    e0 = pixel_extent[0]
+    e1 = pixel_extent[1]
+    we0 = embedding_extent[0]
+    we1 = embedding_extent[1]
+    p0 = pixel_origin[0]
+    p1 = pixel_origin[1]
+    w0 = embedding_origin[0]
+    w1 = embedding_origin[1]
+
+    embedding_to_pixel = np.array([
+        [e0/we0, 0.0, p0-e0*w0/we0],
+        [0.0, -e1/we1, p1+e1*(w1+we1)/we1],
+        [0.0, 0.0, 1.0]
+    ])
+
+    return embedding_to_pixel
